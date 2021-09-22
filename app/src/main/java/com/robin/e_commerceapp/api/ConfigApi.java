ce4e9a7f911cab93997735a2a651006ebe7e68ca
Robin-Casa <sant4n4.1@gmail.com>
2021-09-22 17:15:49 -0500

    Creando el archivo configApi, que nos va servir para comunicarnos con el backend

---
+++ app/src/main/java/com/robin/e_commerceapp/api/ConfigApi.java
@@ -0,0 +1,61 @@
+package com.robin.e_commerceapp.api;
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.robin.e_commerceapp.utils.DateSerializer;
+import com.robin.e_commerceapp.utils.TimeSerializer;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ConfigApi {
+    public static final String baseUrlE = "http://10.0.2.2:8089/Proyecto_Ecommer_Backend_war";
+    //public static final String baseUrlE = "http://localhost:8089/Proyecto_Ecommer_Backend_war";
+    private static Retrofit retrofit;
+    private static String token = "";
+
+    static {
+        initClient();
+    }
+
+    private static void initClient() {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Date.class, new DateSerializer())
+                .registerTypeAdapter(Time.class, new TimeSerializer())
+                .create();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrlE)//Si quieren ejecutar la app desde su móvil, cambiar aquí con la ip de su ordenador
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(getClient())
+                .build();
+    }
+
+    public static OkHttpClient getClient() {
+        HttpLoggingInterceptor loggin = new HttpLoggingInterceptor();
+        loggin.level(HttpLoggingInterceptor.Level.BODY);
+
+        StethoInterceptor stetho = new StethoInterceptor();
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        builder.addInterceptor(loggin)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .addNetworkInterceptor(stetho);
+        return builder.build();
+    }
+
+    public static void setToken(String value) {
+        token = value;
+        initClient();
+    }
+
+}
